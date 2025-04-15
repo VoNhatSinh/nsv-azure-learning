eed5c3a0f3973fec9ddba70c9204f83e59ef5206
nsv <sinh.vo@elca.vn>
2025-04-16 00:42:38 +0700

    correct read file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,8 +26,9 @@ public class HomeController : Controller
             new BlobServiceClient(new Uri("https://nsvstorageaccount001.blob.core.windows.net"),
                 new DefaultAzureCredential());
         var containerClient = blobServiceClient.GetBlobContainerClient("nsv-container");
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sample.txt");
         var blobClient = containerClient.GetBlobClient("uploaded-sample.txt");
-        await using FileStream uploadFileStream = System.IO.File.OpenRead("sample.txt");
+        await using FileStream uploadFileStream = System.IO.File.OpenRead(filePath);
         await blobClient.UploadAsync(uploadFileStream, overwrite: true);
         uploadFileStream.Close();
         return View();
