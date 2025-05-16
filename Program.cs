e92bf7fdf22d8fddc4297fdf9c50ec2a25c9d218
nsv <sinh.vo@elca.vn>
2025-05-16 11:08:41 +0700

    Add application insight connectionstring

--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
+using Microsoft.Extensions.Azure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure logging to output to both Console and Application Insights
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
-builder.Logging.AddApplicationInsights();
+builder.Logging.AddApplicationInsights(configureTelemetryConfiguration: (config) =>
+        config.ConnectionString = builder.Configuration["ApplicationInsights:ConnectionString"],
+    configureApplicationInsightsLoggerOptions: (options) => { }
+);
 
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddApplicationInsightsTelemetry(option =>
-    option.ConnectionString = builder.Configuration["ApplicationInsights:ConnectionString"]);
+{
+    option.ConnectionString = "InstrumentationKey=bead9221-806e-4367-9d8b-ca31689b8b03;IngestionEndpoint=https://eastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://eastasia.livediagnostics.monitor.azure.com/;ApplicationId=c3aebdba-b8ce-42df-87a7-635f8c767e18";
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
